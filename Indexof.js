bfff81d6880798d230efa712c50defa68157fad5
skdding3 <skdding3@gmail.com>
2022-07-31 20:45:21 +0900

    indexOf,lastindexOf

---
+++ Indexof.js
@@ -0,0 +1,10 @@
+// #2 indexOf(), lastindexOf()
+
+const a = [1, 2, 3, 3, 5];
+
+// 순서 , 역순
+console.log(a.indexOf(3)); // 2
+console.log(a.lastIndexOf(3)); // 3
+
+// 두번째 인수 검색
+console.log(a.indexOf(3, 3)); // 3
