696b22fbbce920d135c1a6afcbff1528bc1ba458
skdding3 <skdding3@gmail.com>
2022-08-03 20:40:43 +0900

    #5 forEach

--- forEach.js
+++ forEach.js
@@ -7,3 +7,22 @@ arr.forEach(function (item, index, array) {
 // 첫번째 인수는 배열의 각각 item
 // 두번째는 배열의 index
 // 세번째는 배열
+
+// 가 0 나
+// 나 1 다
+// 다 2 라
+// 라 3 undefined
+
+var a = [1, 2, 3, 4, 5];
+var sum = 0;
+a.forEach(function (value) {
+  sum += value;
+});
+console.log(sum);
+// 15 배열의 합을 구한다.
+
+a.forEach(function (v, i, b) {
+  b[i] = v * v;
+});
+console.log(a);
+// [ 1, 4, 9, 16, 25 ] 각 배열 요소의 제곱
