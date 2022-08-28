bd1ce27006158272e745f935cacdf1e274576b74
skdding3 <skdding3@gmail.com>
2022-08-28 22:57:37 +0900

    JQuery serialize()

--- serialize.js
+++ serialize.js
@@ -1,4 +1,4 @@
-// #11 serialize
+// Jquery serialize
 
 $("input, textarea, select").serialize();
 
@@ -7,4 +7,16 @@ $("form").on("submit", function (event) {
   console.log($(this).serialize());
 });
 
+// ajax를 활용한 실습 코드
+$("#btnSearch_2").click(function () {
+  $.ajax({
+    url: "/admin/member/management",
+    method: "POST",
+    data: $("form[id=account_search]").serialize(),
+    success: function (result) {
+      grid.resetData(result);
+    },
+  });
+});
 
+// grid가 가지고 있는 데이터 cell을 검색할때, form에 있는 value값을 토대로 가져온다.(serialize)
