e5551621f3a34a9a4d1d76ded110f5ccfa21d41b
skdding3 <skdding3@gmail.com>
2022-08-31 08:06:48 +0900

    JQuery ajax (코드 설명 기술)

--- ajax.js
+++ ajax.js
@@ -12,15 +12,16 @@ $('request').click(function () {
 
   // ajax 통신
   $.ajax({
-    type: "POST",
-    url: "/",
-    data: params,
+    type: "POST", // 어떤 방식으로 가져오는가, ex) POST, GET
+    url: "/", // 자원
+    data: params, // 데이터
+    dataType: json, // 넘겨져오는 데이터 형태
     success : function(res) {
       // 응답코드 : 200
       alert(res.code);
     },
     error : function(XMLHttpRequest,textStatus, errorThrown) {
-      alert(falied);
+      alert(falied); // 에러가 났을때 처리할 내용 기술
     }
   })
 })
