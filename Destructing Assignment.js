c8327e54324cddf594bd25b79284a5b4e35deef6
skdding3 <skdding3@gmail.com>
2022-09-18 15:16:39 +0900

    구조분해할당 (URL parsing 배열 할당법)

--- Destructing Assignment.js
+++ Destructing Assignment.js
@@ -7,13 +7,13 @@ let arr = ["Sunki", "Kim"]
 // firstName에는 arr[0]을
 // surName에는 arr[1]을 할당
 
-// let [firstName, surName] = arr;
-//
-// // or
+let [firstName, surName] = arr;
+
+// or
 // let [firstName, surName] = "Sunki Kim".split(' ');
-//
-// console.log(firstName)
-// console.log(surName)
+
+console.log(firstName)
+console.log(surName)
 
 // ES5
 var arr2 = [1, 2, 3];
@@ -34,4 +34,46 @@ const arr3 = [1, 2, 3];
 
 const [one1, two1, three1] = arr3;
 
+
 console.log(one1, two1, three1); // 1, 2, 3
+
+
+
+
+// ex)  url parsing
+
+function parseURL(url = '') {
+    // '://' 앞의 문자열(protocal)rhk '/' 이전의 '/'로 시작하지 않는 문자열(host)과
+    // '/' 이후의 문자열(path)을 검색한다.
+    const parsedURL = url.match(/^(\w+):\/\/([^/]+)\/(.*)$/);
+    console.log(parsedURL);
+    /*
+    [
+     'https://developer.mozilla.org/ko/docs/Web/JavaScript',
+     'https',
+     'developer.mozilla.org',
+     'ko/docs/Web/JavaScript',
+     index: 0,
+     input: 'https://developer.mozilla.org/ko/docs/Web/JavaScript',
+     groups: undefined
+    ]
+     */
+
+    if (!parsedURL) return {};
+
+    // 배열 디스트럭처링 할당을 사용하여 이터러블에서 필요한 요소만 추출한다.
+
+    const [, protocol, host, path] = parsedURL;
+    return { protocol, host, path };
+
+}
+const parsedURL = parseURL('https://developer.mozilla.org/ko/docs/Web/JavaScript');
+
+console.log(parsedURL);
+/*
+{
+  protocol: 'https',
+  host: 'developer.mozilla.org',
+  path: 'ko/docs/Web/JavaScript'
+}
+ */
