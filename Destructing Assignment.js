8a8db2c59cd00e3d06be6b612501d42de92f83ff
skdding3 <skdding3@gmail.com>
2022-09-18 15:41:52 +0900

    구조분해할당 (객체 분해 할당)

--- Destructing Assignment.js
+++ Destructing Assignment.js
@@ -1,6 +1,6 @@
 // 구조분해할당
 
-// 배열 분해하기
+//************ 배열 분해하기 ************/
 let arr = ["Sunki", "Kim"]
 
 // 구조분해할당을 이용해서
@@ -77,3 +77,35 @@ console.log(parsedURL);
   path: 'ko/docs/Web/JavaScript'
 }
  */
+
+
+//************ 객체 분해하기 ************/
+
+// ES5
+var user = {firstName : 'Sunki', lastName : 'Kim'};
+
+var firstName1 = user.firstName;
+var lastName1 = user.lastName;
+
+console.log(firstName1, lastName1); // Sunki Kim
+
+// ES6
+const user2 = { firstName2: 'Sunki', lastName2: 'Kim' };
+const { lastName2, firstName2 } = user2
+
+console.log(firstName2, lastName2); // Sunki Kim
+
+
+// todo 예시
+function printTodo(todo) {
+    console.log(`오늘 ${todo.content}은 ${todo.completed ? '완료' : '진행중'} 상태입니다`);
+}
+printTodo({id: 1, content: '블로깅', completed: true});
+// 오늘 블로깅은 완료 상태입니다.
+
+// todo 예시2
+function printTodo2({ content, completed }) {
+    console.log(`오늘 ${content}은 ${completed ? '완료' : '진행중'} 상태입니다`);
+}
+printTodo2({ id: 1, content: '블로깅', completed: false})
+// 오늘 블로깅은 진행중 상태입니다.
