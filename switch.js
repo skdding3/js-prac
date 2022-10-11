a7a165d144c92ffc0146cd2639d604ac632678d4
skdding3 <skdding3@gmail.com>
2022-10-11 21:50:45 +0900

    Switch Case

---
+++ switch.js
@@ -0,0 +1,20 @@
+// switch 문
+
+// 하나 이상의 case에서 작동한다.
+
+let a = 2 + 2;
+
+switch (a) {
+    case 3:
+        console.log( '비교하려는 값보다 작습니다.' );
+        break;
+    case 4:
+        console.log( '비교하려는 값과 일치합니다.' );
+        break;
+    case 5:
+        console.log( '비교하려는 값보다 큽니다.' );
+        break;
+    default:
+        console.log( "어떤 값인지 파악이 되지 않습니다." );
+}
+// 비교하려는 값과 일치합니다.
