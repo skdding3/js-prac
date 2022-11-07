e45a6a4b19ffe53af1533cac3fd2be2d8b7bba6a
skdding3 <skdding3@gmail.com>
2022-11-08 08:07:38 +0900

    Promise

--- Promise.js
+++ Promise.js
@@ -1,24 +1,47 @@
 // Promise
 
+//
+// const pr = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         // resolve('ok')
+//         reject(new Error('error'))
+//     }, 3000)
+// })
 
-const pr = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // resolve('ok')
-        reject(new Error('error'))
-    }, 3000)
-})
+
+// pr.then(
+//     function (result) {
+//         console.log(result + '가지러 가자')
+//     }
+// ).catch(
+//     function (err) {
+//         console.log('다시 주문해주세요')
+//     }
+// ).finally(
+//     function (){
+//         console.log('order end')
+//     }
+// )
 
 
-pr.then(
-    function (result) {
-        console.log(result + '가지러 가자')
+function getPro(name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(name)
+        }, 1000)
+    })
+}
+
+async function getPr2() {
+    try {
+        const result = await getPro('pro');
+        console.log(result)
+    } catch (e) {
+        console.log(e)
     }
-).catch(
-    function (err) {
-        console.log('다시 주문해주세요')
-    }
-).finally(
-    function (){
-        console.log('order end')
-    }
-)
+
+}
+
+console.log('시작');
+getPr2()
+
