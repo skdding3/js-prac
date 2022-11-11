a1b0a81112bad061333a7d72e1e32c51dfd7c9aa
skdding3 <skdding3@gmail.com>
2022-11-11 17:11:42 +0900

    setTimeout 면접 질문

--- setTimeout.js
+++ setTimeout.js
@@ -1,24 +1,35 @@
-// setTimeout
+// // setTimeout
+//
+// function showName(name) {
+//     console.log(name);
+// }
+//
+// // setTimeout(함수, 시간, 인수);
+//
+// setTimeout(showName, 2000, 'Sunki');
+//
+//
+// // setInterval
+//
+// let num = 0;
+//
+// function showTime() {
+//     console.log(`안녕하세요 접속한지 ${num++}초가 지났습니다.`)
+//     if (num > 5) {
+//         clearInterval(tId)
+//     }
+// }
+//
+// const tId = setInterval(showTime, 1000)
+//
 
+
+// 면접 질문
+
+var name = 'Sunki';
 function showName(name) {
-    console.log(name);
+    console.log(name)
 }
 
-// setTimeout(함수, 시간, 인수);
-
-setTimeout(showName, 2000, 'Sunki');
-
-
-// setInterval
-
-let num = 0;
-
-function showTime() {
-    console.log(`안녕하세요 접속한지 ${num++}초가 지났습니다.`)
-    if (num > 5) {
-        clearInterval(tId)
-    }
-}
-
-const tId = setInterval(showTime, 1000)
-
+setTimeout(showName,1000,'Sunki')
+console.log(name)
