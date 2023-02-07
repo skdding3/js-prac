4ba70c858328c3a2f9b7be3858c230fdbaf3d9e8
skdding3 <skdding3@gmail.com>
2023-02-07 20:50:14 +0900

    변수

---
+++ variable.js
@@ -0,0 +1,16 @@
+// 변수 (variable)
+
+
+let name;
+name = 'sunki'
+
+var age;
+age = 30
+
+const gender;
+gender = 'male'
+
+
+console.log(name) // sunki
+console.log(age) // 30
+console.log(gender) // SyntaxError: Missing initializer in const declaration
