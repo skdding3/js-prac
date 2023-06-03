8762138fe88d3671b646c4a7b51c6ebe92977ed9
skdding3 <skdding3@gmail.com>
2023-06-03 16:01:41 +0900

    this case

--- this.js
+++ this.js
@@ -23,3 +23,31 @@ const climb = {
 
 climb.showTitle(); // bolder -> 객체 메소드
 
+// 객체 메소드의 함수 this
+
+const fn = {
+    title: 'climbing',
+    tags: ['red','orange','yellow', 'green'],
+    showTags() {
+        this.tags.forEach(function (tag) {
+            console.log(tag);
+            console.log(this)
+        })
+    }
+}
+
+// fn.showTags()
+
+
+// 객체 메소드 함수 this 해결방안
+const fn2 = {
+    title: 'climbing',
+    tags: ['red', 'orange', 'yellow', 'green'],
+    showTags() {
+        this.tags.forEach(function(tag) {
+            console.log(tag);
+            console.log(this); // fn
+        }, this); // 여기는 일반 함수 바깥, fn 객체를 참조할 수 있다.
+    }
+}
+fn2.showTags();
