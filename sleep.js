e0be773231c58d5fd807fa564c5150e4e23cb796
skdding3 <skdding3@gmail.com>
2023-08-26 18:36:13 +0900

    Sleep finally

--- sleep.js
+++ sleep.js
@@ -3,51 +3,51 @@
 // 동기 지연  별도 기능이 아니므로 직접 구현
 
 function sleep(ms) {
-    const wakeUpTime = Date.now() + ms
-    while (Date.now() < wakeUpTime) {
+    const wakeUp = Date.now() + ms
+    while (Date.now() < wakeUp) {
     }
 }
 
 
 function test() {
-    console.log('before')
+    console.log('시작')
     sleep(2000)
-    console.log('after')
+    console.log('동작')
 }
 
 console.log('-- 동기 지연 --')
-test() // before  -> after
+test() // 시작  -> 동작
 
 
 // 비동기 지연
 
-// function sleep2(ms) {
-//     return new Promise((res) => setTimeout(res, ms))
-// }
-//
-// function test2() {
-//     console.log('before')
-//     sleep2(2000).then(() => console.log('after'))
-//     console.log('done!')
-// }
+function sleep2(ms) {
+    return new Promise((res) => setTimeout(res, ms))
+}
 
-// console.log('-- 비동기 지연 --')
-// test2() // before --> done! --> after
+function test2() {
+    console.log('시작')
+    sleep2(2000).then(() => console.log('동작'))
+    console.log('끝!')
+}
+
+console.log('-- 비동기 지연 --')
+test2() // 시작 --> 끝! --> 동작
 
 
-// function sleep3(ms) {
-//     return new Promise((res) => setTimeout(res, ms))
-// }
-//
-// function test3() {
-//     console.log('before')
-//     sleep3(2000)
-//         .then(() => console.log('after'))
-//         .then(() => console.log('done!'))
-// }
-//
-// console.log('-- 비동기 지연 수정 --')
-// test3() // before -> after -> done
+function sleep3(ms) {
+    return new Promise((res) => setTimeout(res, ms))
+}
+
+function test3() {
+    console.log('시작')
+    sleep3(2000)
+        .then(() => console.log('동작'))
+        .then(() => console.log('끝!'))
+}
+
+console.log('-- 비동기 지연 수정 --')
+test3() // before -> after -> done
 
 
 // 비동기 지연 async await
@@ -57,11 +57,11 @@ function sleep4 (ms) {
 }
 
 async function test4() {
-    console.log('before')
+    console.log('시작')
     await sleep4(4000)
-    console.log('after')
+    console.log('동작')
 }
 
 console.log('-- 비동기 지연 async/await --')
 test4()
-console.log('done!')
+console.log('끝!')
